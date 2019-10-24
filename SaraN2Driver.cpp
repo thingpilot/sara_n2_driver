2bc6915898cd206a176049f2dc13cbd454494af4
Adam <adam@think-engineer.com>
2019-10-24 11:21:27 +0100

    Initial commit of SaraN2Driver files

---
+++ SaraN2Driver.cpp
@@ -0,0 +1,21 @@
+/**
+  * @file    SaraN2Driver.cpp
+  * @version 0.1.0
+  * @author  Adam Mitchell
+  * @brief   C++ file of the NB-IoT driver module
+  */
+
+/** Includes
+ */
+#include "SaraN2Driver.h"
+
+
+SaraN2::SaraN2()
+{
+
+}
+
+SaraN2::~SaraN2()
+{
+
+}
---
+++ SaraN2Driver.h
@@ -0,0 +1,30 @@
+/**
+  * @file    SaraN2Driver.h
+  * @version 0.1.0
+  * @author  Adam Mitchell
+  * @brief   Header file of the  NB-IoT driver module
+  */
+
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes 
+ */
+#include <mbed.h>
+#include "board.h"
+
+
+class SaraN2
+{
+
+	public:
+
+		SaraN2();
+
+		~SaraN2();
+
+	private:
+
+};
+
